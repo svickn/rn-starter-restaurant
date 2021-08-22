d422b84286a997381a7f4f0452dae8426a313440
Stacy Vicknair <svickn@gmail.com>
2021-08-22 17:33:19 -0500

    Section 10: Lesson 105

---
+++ src/hooks/useResults.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import yelp from '../api/yelp'
+
+const statusOptions = {
+  loading: 'loading',
+  idle: 'idle',
+  error: 'error',
+}
+
+export default () => {
+  const [results, setResults] = React.useState([])
+  const [error, setError] = React.useState()
+  const [status, setStatus] = React.useState(statusOptions.loading)
+
+  const searchApi = async searchTerm => {
+    setStatus(statusOptions.loading)
+    setError('')
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: 'baton rouge, la',
+        },
+      })
+
+      setResults(response.data.businesses)
+      setStatus(statusOptions.idle)
+    } catch (error) {
+      setError('Something went wrong!')
+      setStatus(statusOptions.error)
+    }
+  }
+
+  React.useEffect(() => {
+    searchApi('pasta')
+  }, [])
+
+  return {searchApi, results, status, error, statusOptions}
+}
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,33 +1,11 @@
 import React from 'react'
 import {Text, StyleSheet, View} from 'react-native'
 import SearchBar from '../components/SearchBar'
-import yelp from '../api/yelp'
+import useResults from '../hooks/useResults'
 
 const SearchScreen = () => {
   const [term, setTerm] = React.useState('')
-  const [results, setResults] = React.useState([])
-  const [errorMsg, setErrorMsg] = React.useState()
-
-  const searchApi = async searchTerm => {
-    setErrorMsg('')
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term,
-          location: 'baton rouge, la',
-        },
-      })
-
-      setResults(response.data.businesses)
-    } catch (error) {
-      setErrorMsg('Something went wrong!')
-    }
-  }
-
-  React.useEffect(() => {
-    searchApi('pasta')
-  }, [])
+  const {searchApi, results, error, status, statusOptions} = useResults()
 
   return (
     <View>
@@ -36,8 +14,11 @@ const SearchScreen = () => {
         onTermChange={setTerm}
         onTermSubmit={() => searchApi(term)}
       />
-      {errorMsg ? <Text>{errorMsg}</Text> : null}
-      <Text>We have found {results.length} results.</Text>
+      {status === statusOptions.error ? <Text>{error}</Text> : null}
+      {status === statusOptions.loading ? <Text>Loading...</Text> : null}
+      {status === statusOptions.idle ? (
+        <Text>We have found {results.length} results.</Text>
+      ) : null}
     </View>
   )
 }
