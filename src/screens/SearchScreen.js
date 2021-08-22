ddcbaec1db44e31a7c63d1ef6daf2bb8fd547d08
Stacy Vicknair <svickn@gmail.com>
2021-08-22 17:16:02 -0500

    Section 10: Lesson 104

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,21 +6,37 @@ import yelp from '../api/yelp'
 const SearchScreen = () => {
   const [term, setTerm] = React.useState('')
   const [results, setResults] = React.useState([])
+  const [errorMsg, setErrorMsg] = React.useState()
 
-  const searchApi = async () => {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term,
-        location: 'baton rouge, la',
-      },
-    })
-    setResults(response.data.businesses)
+  const searchApi = async searchTerm => {
+    setErrorMsg('')
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term,
+          location: 'baton rouge, la',
+        },
+      })
+
+      setResults(response.data.businesses)
+    } catch (error) {
+      setErrorMsg('Something went wrong!')
+    }
   }
+
+  React.useEffect(() => {
+    searchApi('pasta')
+  }, [])
 
   return (
     <View>
-      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
+      {errorMsg ? <Text>{errorMsg}</Text> : null}
       <Text>We have found {results.length} results.</Text>
     </View>
   )
