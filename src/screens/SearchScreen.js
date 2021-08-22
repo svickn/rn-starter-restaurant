ee8edc0fda96616cba38624f7fa7fc81b3631e1a
Stacy Vicknair <svickn@gmail.com>
2021-08-22 16:58:54 -0500

    Section 9: Lesson 100

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import {Text, StyleSheet, View} from 'react-native'
 import SearchBar from '../components/SearchBar'
+import yelp from '../api/yelp'
 
 const SearchScreen = () => {
   const [term, setTerm] = React.useState('')
+  const [results, setResults] = React.useState([])
+
+  const searchApi = async () => {
+    const response = await yelp.get('/search', {
+      params: {
+        limit: 50,
+        term,
+        location: 'baton rouge, la',
+      },
+    })
+    setResults(response.data.businesses)
+  }
 
   return (
     <View>
-      <SearchBar
-        term={term}
-        onTermChange={setTerm}
-        onTermSubmit={() => console.log('submitted!')}
-      />
-      <Text>{term}</Text>
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      <Text>We have found {results.length} results.</Text>
     </View>
   )
 }
